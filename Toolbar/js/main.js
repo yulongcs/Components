11d3c210fbfe084072cb0141f595adf05ed7fb5f
yulongcs <yulongcs@qq.com>
2015-07-02 00:06:28 +0800

    add index3 and js
    1. :before， :after 方式
    2. js backtop 插件  requireJS编写

---
+++ Toolbar/js/main.js
@@ -0,0 +1,20 @@
+requirejs.config({
+	paths:{
+		jquery:'jquery-2.1.4.min'
+	}
+});
+
+requirejs(['jquery','backtop'], function($, backtop){
+
+	// new backtop.BackTop($('#backTop'),{
+	// 	mode: 'go',
+	// 	pos:100,
+	//     speed:1000
+	// });
+$('#backTop').backtop({
+	mode: 'move',
+	pos:100,
+	speed:1000
+});
+
+});
